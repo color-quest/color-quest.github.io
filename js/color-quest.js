8ba36eeb831d54fd2b55b35a4dd0c141006cfad0
color-quest <co-st.transport541@passinbox.com>
2025-02-22 17:06:03 +0100

    Introduce player sprites

--- js/color-quest.js
+++ js/color-quest.js
@@ -1,24 +1,62 @@
 
 // Global variables
-var shared;
+var shared = { x: 100, y: 100 };
+var playerImg = null;
+var frame = 0;
+
+
+// Sprite class
+// img should be Image object which contains several pictures in line
+// in the 4 directions: Left, Right, Up and Down (4 different lines)
+// First image in each row is the idle position
+function Sprite(img, frames, x, y, scale)
+{
+	this.img = img;
+	this.scale = scale;
+	this.x = x;
+	this.y = y;
+	this.w = img.width / frames;
+	this.h = img.height / 4;
+	this.frame = 0;
+	this.frames = frames;
+	
+	this.Draw = function(dir)
+	{
+		image(this.img, this.x, this.y, this.w * this.scale, this.h * this.scale, this.w * this.frame, 0, this.w, this.h)
+
+		this.frame += 1;
+		if(this.frame > this.frames)
+		{
+			this.frame = 0;
+		}
+	}
+}
+
+
 
 function preload()
 {
-	// connect to the deepstream.io websocket server
-	partyConnect("wss://ws-0iff.onrender.com/deepstream", "test");
+	// Load player image
+	playerImg = loadImage('./assets/player.png');
+	
+	// Connect to the deepstream.io websocket server on Render.com
+	partyConnect("wss://ws-0iff.onrender.com/deepstream", "color-quest");
 
-	// begin loading shared object
-	// and provide starting values for the object to be used
-	// if there are no clients already connected to the room
-	// setup() won't be called until the shared object is loaded
-	shared = partyLoadShared("shared", { x: 100, y: 100 });
+	// Begin loading shared object
+	shared = partyLoadShared("shared", shared);
 }
 
 function setup()
 {
-	createCanvas(400, 400);
+	createCanvas(displayWidth, displayHeight);
+	background(0);
 	noStroke();
-	partyToggleInfo(true);
+	
+	// Player
+	player = new Sprite(playerImg, 0, 0, 4);
+	
+	// Display info?
+	//partyToggleInfo(true);
 }
 
 function mousePressed()
@@ -30,9 +68,11 @@ function mousePressed()
 
 function draw()
 {
-	background("#ffcccc");
-	fill("#000066");
+	background(100);
 
-	// read shared data
-	ellipse(shared.x, shared.y, 100, 100);
+	player.x = mouseX;
+	player.Draw();
+	
+	// Draw circle from shared data
+	ellipse(shared.x, shared.y, 50, 50);
 }
